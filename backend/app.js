5538f70077b179b9e3d64872d356b1a9abb0cc5e
Rohitashwa Pal Das <rohit090505pal@gmail.com>
2025-02-15 23:52:36 +0530

    Updated

--- backend/app.js
+++ backend/app.js
@@ -95,7 +95,7 @@ const generateUsername = (email) => {
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://rythmix-backend-vask.onrender.com/auth/google/callback",
     scope: ['profile', 'email'],
   },
   async (accessToken, refreshToken, profile, done) => {
